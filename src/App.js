52a161ce76fd17d49d2a438ba0dbbcfee20f6fef
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-07-10 14:19:43 +0530

    landing page plus api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   const renderComponent = () => {
     return (
       <>
         <Navbar />
-        <div>LandingPage</div>
+        <LandingPage />
       </>
     );
   };
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -2,7 +2,27 @@ import React from "react";
 import "../Style/LandingPage.css";
 
 const LandingPage = () => {
-  return <div></div>;
+  const data = ["All Products", "Electronics", "Sports", "Kitchenware"];
+  return (
+    <div>
+      <div className="headig">
+        <h4>Welcome To Online Shopping</h4>
+      </div>
+      <div className="container">
+        <div className="container_products">
+          <div className="col">
+            <div className="row">
+              {data.map((category, index) => (
+                <div className="main_container" key={index}>
+                  <a href="/">{category}</a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
